2f53cbd2aabd3d35b0a9845c68ae2b5b15ca681b
novinbukannopin <novinardiany@outlook.com>
2024-01-23 21:44:10 +0700

    fix<footer> : rename footer again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => {
                 </div>
 
                 <div className={"border bg-gray-20"}/>
-                <p className={"regular-14 w-full text-center text-gray-30"}>2024 Novin | All right reserved</p>
+                <p className={"regular-14 w-full text-center text-gray-30"}>2024 Novin Imut | All right reserved</p>
             </div>
         </footer>
     )
